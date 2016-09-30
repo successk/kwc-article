6be35d409fb14fe1d056b76076d31f1ee8f8d3fa
Gaëtan Rizio <gaetan@rizio.fr>
2016-09-30 21:34:30 +0200

    Adds summary generation.

---
+++ src/kwc-article-summary.js
@@ -0,0 +1,41 @@
+(() => {
+  "use strict";
+
+  class KwcArticleSummary {
+    beforeRegister() {
+      this.is = "kwc-article-summary";
+
+      this.properties = {
+        items: {
+          type: Array,
+          value: [],
+          observer: "_itemsChanged"
+        }
+      };
+    }
+
+    _itemsChanged(items) {
+      const $root = Polymer.dom(this.$.root);
+      $root.childNodes.forEach((child) => $root.removeChild(child));
+      if (items && items.length > 0) {
+        items.forEach((item) => $root.appendChild(this.buildItem(item)));
+      }
+    }
+
+    buildItem(item) {
+      const li = document.createElement("li");
+      const a = document.createElement("a");
+      a.setAttribute("href", `#${item.id}`);
+      a.appendChild(document.createTextNode(item.name));
+      li.appendChild(a);
+      if (item.children && item.children.length > 0) {
+        const ul = document.createElement("ol");
+        li.appendChild(ul);
+        item.children.forEach((child) => ul.appendChild(this.buildItem(child)));
+      }
+      return li;
+    }
+  }
+
+  Polymer(KwcArticleSummary);
+})();
--- src/kwc-article.js
+++ src/kwc-article.js
@@ -1,12 +1,48 @@
 (() => {
   "use strict";
-  
+
   class KwcArticle {
     beforeRegister() {
       this.is = "kwc-article";
 
-      this.properties = {
-      };
+      this.properties = {};
+    }
+
+    attachedCallback() {
+      const summary = this.querySelector("kwc-article-summary");
+      if (summary) {
+        const titleNodes = this.querySelectorAll(".kwc-article-text h2, .kwc-article-text h3, .kwc-article-text h4, .kwc-article-text h5, .kwc-article-text h6");
+        const summaryItems = [];
+        const flatItems = [];
+
+        titleNodes.forEach((titleNode)=> {
+          if (!titleNode.hasAttribute("id") || titleNode.getAttribute("id").trim() === "") {
+            const canonical = titleNode.textContent
+              .toLowerCase()
+              .replace(/[^a-zA-Z0-9+_-]/gi, "-")
+              .replace(/-+/gi, "-");
+            titleNode.setAttribute("id", canonical);
+          }
+        });
+
+        titleNodes.forEach((titleNode) => {
+          const level = parseInt(titleNode.tagName.substr(1), 10);
+          const item = {level: level, name: titleNode.textContent, id: titleNode.getAttribute("id"), children: []};
+          if (level > 2) {
+            for (let i = flatItems.length - 1; i >= 0; i--) {
+              if (flatItems[i].level !== level) {
+                flatItems[i].children.push(item);
+                break;
+              }
+            }
+          } else {
+            summaryItems.push(item);
+          }
+          flatItems.push(item);
+        });
+
+        summary.setAttribute("items", JSON.stringify(summaryItems));
+      }
     }
   }
 
